e2351b02e1bcd77b8159368abde1ca5c5cd36eb0
Minakho95 <minakho95@gmail.com>
2021-05-16 23:45:12 +0200

    comics update

--- routes/comics.js
+++ routes/comics.js
@@ -6,9 +6,10 @@ router.get("/comics", async (req, res) => {
   try {
     const limit = req.query.limit || 100;
     const skip = req.query.skip || 0;
+    const title = req.query.title && `&title=${req.query.title}`;
 
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&${title}`
     );
     res.status(200).json(response.data);
   } catch (error) {
